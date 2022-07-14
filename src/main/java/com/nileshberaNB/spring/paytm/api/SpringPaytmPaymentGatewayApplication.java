86a968fc6eef7a85144468cba5b0408296893065
nileshberaNB <nileshbera3012@gmail.com>
2022-07-14 15:48:30 +0530

    first commit

---
+++ src/main/java/com/nileshberaNB/spring/paytm/api/SpringPaytmPaymentGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nileshberaNB.spring.paytm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaytmPaymentGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaytmPaymentGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nileshberaNB/spring/paytm/api/SpringPaytmPaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nileshberaNB.spring.paytm.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaytmPaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
